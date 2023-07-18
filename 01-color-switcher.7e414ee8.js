9257bef845c17a1256c688da2dfb962138235ee3
bajmutov <bajmutov@gmail.com>
2023-07-18 12:34:35 +0000

    Deploying to gh-pages from @ bajmutov/goit-js-hw-09@5ad9ac587e649dbe0b65d89fb57e2289182c3bea 🚀

---
+++ 01-color-switcher.7e414ee8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;let a=null;t.addEventListener("click",(function(){a=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.7e414ee8.js.map
