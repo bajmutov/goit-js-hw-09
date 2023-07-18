5ad9ac587e649dbe0b65d89fb57e2289182c3bea
Oleksandr <bajmutov@gmail.com>
2023-07-18 15:33:47 +0300

    1dz

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bgc = document.body;
+let timerId = null;
 
+startBtn.addEventListener('click', onChangeBgkClick);
+stopBtn.addEventListener('click', onStopChangeBgk);
+
+function onChangeBgkClick() {
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    bgc.style.backgroundColor = color;
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopChangeBgk() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
