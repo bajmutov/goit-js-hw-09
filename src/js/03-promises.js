4f32510dac384b9186cbb390e88c75d0e3f11620
Oleksandr <bajmutov@gmail.com>
2023-07-23 19:47:01 +0300

    last

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,32 +11,30 @@ function handleSubmit(event) {
   } = event.currentTarget;
   console.log(delay.value, step.value, amount.value);
 
-  for (let i = 0; i < amount.value; i += 1) {
-    setTimeout(createPromise, delay.value, i, step.value, delay.value);
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delay.value)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay.value = Number(delay.value) + Number(step.value);
+    console.log(delay.value);
   }
-
   event.currentTarget.reset();
 }
 
-function createPromise(position, step, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(() => {
-    if (shouldResolve) {
-      return Promise.resolve(
-        Notify.success(
-          `✅ Fulfilled promise ${position + 1} in ${
-            Number(delay) + position * step
-          } ms`
-        )
-      );
-    } else {
-      return Promise.reject(
-        Notify.failure(
-          `❌ Rejected promise ${position + 1} in ${
-            Number(delay) + position * step
-          } ms`
-        )
-      );
-    }
-  }, position * step);
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
